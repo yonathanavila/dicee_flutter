1a6dda0d917bd4e467aac9dc60a63025079ee6a0
Yonathan Cruz <ycruz@senprende.hn>
2024-01-03 14:06:47 -0600

    add statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,58 @@ void main() {
       useMaterial3: true,
     ),
     home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.grey,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: const Text('Dicee'),
+          backgroundColor: Colors.blueGrey,
         ),
-        body: DicePage()),
+        body: const DicePage()),
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   // This widget is the root of your application.
+
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+                child: Image.asset('images/$leftDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = 5;
+                  });
+                }),
+          )),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              child: Image.asset('images/$rightDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = 3;
+                });
+              },
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
